b611acf50a296dba056bd078e72e9478d0ddec59
Lorenzo Castellino <castellino96@gmail.com>
2024-06-26 12:13:19 +0200

    Update mousePressEvent in CustomComboBox to fix focus issue

--- src/main/python/pyes/ui/widgets/combobox.py
+++ src/main/python/pyes/ui/widgets/combobox.py
@@ -1,3 +1,4 @@
+from PySide6.QtGui import QMouseEvent
 from PySide6.QtWidgets import QComboBox, QWidget
 
 
@@ -6,6 +7,6 @@ class CustomComboBox(QComboBox):
         super().__init__(parent)
         self.previous_index = self.currentIndex()
 
-    def focusInEvent(self, event):
+    def mousePressEvent(self, e: QMouseEvent) -> None:
         self.previous_index = self.currentIndex()
-        QComboBox.focusInEvent(self, event)
+        return super().mousePressEvent(e)
