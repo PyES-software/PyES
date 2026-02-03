eaf3a89d84171430887d1439758b7a952ef3c9d8
Salvador Blasco <salvador.blasco@protonmail.com>
2026-02-03 15:50:47 +0100

    [fix] removed unwanted edition of _data

--- src/main/python/pyes/ui/widgets/input.py
+++ src/main/python/pyes/ui/widgets/input.py
@@ -138,7 +138,8 @@ class inputTitrationOpt(QWidget, Ui_inputTitrationOpt):
                     c.model()._data = pd.DataFrame(data[c.objectName()])
                     if c.objectName() == "titrationView":
                         c.model().setColumnReadOnly([3], True)
-                        c.model()._data["pX"] = 0.0
+                        #c.model()._data.iloc[:, 4] = 0.0
+                        #c.model()._data["pX"] = 0.0
                         c.model().update_pX(
                             e0=self.e0.value(), slope=self.slope.value()
                         )
